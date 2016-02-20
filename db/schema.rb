1b21c916f7fbd0fbdfc0c048500917edf912707c
Alain Fontaine <criticerz@gmail.com>
2016-02-20 00:20:26 -0500

    its running bro

--- db/schema.rb
+++ db/schema.rb
@@ -14,31 +14,32 @@
 ActiveRecord::Schema.define(version: 20160220050213) do
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "username"
-    t.string   "name"
-    t.string   "company"
-    t.string   "blog"
-    t.string   "location"
+    t.string   "username",     limit: 255
+    t.string   "name",         limit: 255
+    t.string   "company",      limit: 255
+    t.string   "blog",         limit: 255
+    t.string   "location",     limit: 255
     t.boolean  "hireable"
-    t.string   "email"
-    t.string   "bio"
-    t.integer  "public_repos"
-    t.integer  "public_gists"
-    t.integer  "followers"
-    t.integer  "following"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "user_id"
+    t.string   "email",        limit: 255
+    t.string   "bio",          limit: 255
+    t.integer  "public_repos", limit: 4
+    t.integer  "public_gists", limit: 4
+    t.integer  "followers",    limit: 4
+    t.integer  "following",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "user_id",      limit: 4
   end
 
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
