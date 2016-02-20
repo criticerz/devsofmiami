faa84e458300ab680e59c6d9b994b71aceb2f7f6
Todd Metheny <todd.metheny@gmail.com>
2016-02-20 03:59:28 -0500

    added migration to update

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220050213) do
+ActiveRecord::Schema.define(version: 20160220081312) do
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "username",     limit: 255
-    t.string   "name",         limit: 255
-    t.string   "company",      limit: 255
-    t.string   "blog",         limit: 255
-    t.string   "location",     limit: 255
+    t.string   "username"
+    t.string   "name"
+    t.string   "company"
+    t.string   "blog"
+    t.string   "location"
     t.boolean  "hireable"
     t.string   "email"
     t.string   "bio"
@@ -29,29 +29,17 @@ ActiveRecord::Schema.define(version: 20160220050213) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
+    t.string   "avatar_url"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
-    t.string   "email",        limit: 255
-    t.string   "bio",          limit: 255
-    t.integer  "public_repos", limit: 4
-    t.integer  "public_gists", limit: 4
-    t.integer  "followers",    limit: 4
-    t.integer  "following",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",      limit: 4
-  end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "profiles", "users"
 end
