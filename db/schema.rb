ed2f6d93b871114c18cd2395e3f7eae51fb00905
Todd Metheny <todd.metheny@gmail.com>
2016-03-12 17:01:07 -0500

    added admin so I can test the SO thing..won't work local

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229144847) do
+ActiveRecord::Schema.define(version: 20160312215842) do
 
   create_table "code_wars_data", force: :cascade do |t|
     t.string   "username"
@@ -83,11 +83,12 @@ ActiveRecord::Schema.define(version: 20160229144847) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "email"
     t.string   "username"
     t.string   "remember_token"
+    t.boolean  "admin",          default: false
   end
 
 end
